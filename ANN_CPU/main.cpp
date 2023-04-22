3c4eb74691d1d1163cd56b03580e8d082bce2c49
saccharineboi <saccharineboi@gmail.com>
2023-04-23 01:00:51 +0300

    Add license header

--- ANN_CPU/main.cpp
+++ ANN_CPU/main.cpp
@@ -1,3 +1,19 @@
+// Preprocessor: A tool to pre-process images in the pneumonia dataset
+// Copyright (C) 2023 saccharineboi
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <cstdio>
 #include <vector>
 #include <cstdint>
@@ -283,7 +299,10 @@
         constexpr std::size_t hiddenLayerNeuronCount{ 30 };
         constexpr std::size_t hiddenLayerCount{ 5 };
 
-        ANN ann(inputNeuronCount, outputNeuronCount, hiddenLayerNeuronCount, hiddenLayerCount);
+        ANN ann(inputNeuronCount,
+                outputNeuronCount,
+                hiddenLayerNeuronCount,
+                hiddenLayerCount);
 
         ann.ForwardPass(Sigmoid);
 
